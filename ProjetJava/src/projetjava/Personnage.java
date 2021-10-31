5c2b3c67761b8d0a206f28f87f9ae5b44f5a63bb
lyse05494 <92745950+lyse05494@users.noreply.github.com>
2021-10-31 20:58:56 +0100

    Personnage

--- ProjetJava/src/projetjava/Personnage.java
+++ ProjetJava/src/projetjava/Personnage.java
@@ -9,5 +9,34 @@ package projetjava;
  * @author I555807
  */
 public class Personnage {
+    String nom;
+    int xPersonnage, yPersonnage;
+    int xPersonnage2, yPersonnage2;
     
+    public String getNom(){
+        return nom;
+    }
+    public int getxPerso(){
+        return xPersonnage;
+    }
+    public int getyPerso(){
+        return yPersonnage;
+    }
+    public String setNom(String nom){
+       return this.nom = nom; 
+    }
+    public int setxPerso(int xPos){
+        return xPersonnage = xPos;
+    }
+    public int setyPerso(int yPos){
+        return yPersonnage = yPos;
+    }
+    
+    public void collison(){
+        if(xPersonnage == xPersonnage2 && yPersonnage == yPersonnage2 ){
+            System.out.println(" Game Over");
+            
+        }
+    }
 }
+    
